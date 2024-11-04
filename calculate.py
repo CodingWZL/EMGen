2027c8e39e1f8a37334bd155e615f21d50fe4942
Zhilong Wang <104205506+CodingWZL@users.noreply.github.com>
2024-11-04 15:15:08 -0500

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -17,16 +17,10 @@ def main():
     index = []
     features = []
     for i in range(1,116753):
-        #if i in [69,79,81,362,380,459,573,813,879,1073,\
-        #         1099,1225,1532,1536,1537,1544,1550,1565,\
-        #         1571,1706,1710,2413,2761,2762,2763,2883,\
-        #         2909,2945,2946,3010,3124,3129,3137,3141,\
-        #         3144,3145,3147,3155,3159,3164,3167,3175]:
-        #    continue
+       
         structure = Structure.from_file("../../MP-database/cif/" + str(int(i)) + ".cif")
         #structure = Structure.from_file("../cif/" + str(i) + ".cif")
-        #a = f.readline()
-        #print(a)
+       
         ###### Structure Feature ######
         #symmetry_information = structure_symmetry(structure) # dimension:4
         crystal_system, centrosymmetric = structure_symmetry(structure)
@@ -45,11 +39,7 @@ def main():
         a = structure.composition
         #print(int(cif_number[i]))
         elemental_property = meredig(Composition(a))
-        #try:
-        #    site_feature = list(Li_fraction(Composition(a))) # dimension:12
-        #except:
-        #    index.append(i)
-        #    continue
+       
         try:
             bandcenter = band_center(Composition(a)) # dimension:1
         except:
@@ -66,17 +56,13 @@ def main():
         feature.append(average_atom_volume)
         # 1-element
         feature.append(average_volume_atom)
-        #feature.append(neighbor_num)
-        #feature.append(bond_length)
-        #feature.append(bond_angle)
-        #feature.append(effective_ratio)
+      
         # 17-element 1-element
         feature = feature + elemental_property + bandcenter #site_feature# + bandcenter
 
         #feature.append(bandcenter)
         features = features + feature
-        #print(i)
-        #print(i)
+       
         #print(site_feature)
     #f.close()
         print(i)
